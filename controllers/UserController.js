7b2fc643a6025c5483bbe7381685ee830b5d93e0
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-03-20 10:35:28 -0300

    feat: getters, setters and Util class

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,12 +7,21 @@ class UserController{
     onSubmit(){
         this.formEl.addEventListener("submit", (event) => {
             event.preventDefault();
+            let btn = this.formEl.querySelector("[type=submit]");
+            btn.disabled = true;
+
             let values = this.getValues();
-            
-            this.getPhoto((content)=>{
-                values.photo = content;
-                this.addLine(values);
-            });
+            this.getPhoto().then(
+                (content)=>{
+                    values.photo = content;
+                    this.addLine(values);
+                    this.formEl.reset();
+                    btn.disabled = false;
+                }, 
+                (e)=>{
+                    console.error(e);
+                }
+            );
             
         });
     }
@@ -28,6 +37,8 @@ class UserController{
                 if(field.checked === true){
                     user[field.name] = field.value;
                 }
+            } else if(field.name == "admin"){
+                user[field.name] = field.checked;
             } else {
                 user[field.name] = field.value;
             }
@@ -44,8 +55,8 @@ class UserController{
                         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                         <td>${dataUser.name}</td>
                         <td>${dataUser.email}</td>
-                        <td>${dataUser.admin}</td>
-                        <td>${dataUser.birth}</td>
+                        <td>${(dataUser.admin) ? "Sim" : "Não"}</td>
+                        <td>${Utils.dateFormat(dataUser.register)}</td>
                         <td>
                           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                           <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
@@ -56,18 +67,32 @@ class UserController{
     
     }
 
-    getPhoto(callback){
-        let fileReader = new FileReader();
-        let elements = [...this.formEl.elements].filter(item =>{
-            if(item.name === "photo"){
-                return item;
+    getPhoto(){
+        return new Promise((resolve, reject)=>{
+            let fileReader = new FileReader();
+            let elements = [...this.formEl.elements].filter(item =>{
+                if(item.name === "photo"){
+                    return item;
+                }
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = ()=>{
+                resolve(fileReader.result);
             }
-        });
-        let file = elements[0].files[0];
-        fileReader.onload = ()=>{
-            callback(fileReader.result);
-        }
-        fileReader.readAsDataURL(file);
+            fileReader.onerror = (e)=>{
+                reject(e);
+            }
+            
+            if(file){
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg.');
+            }
+            
+        })
+        
     }
 }
 
