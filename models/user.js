833709f9c67d682e963e9a54c6cd69eaba1ebed8
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-03-21 11:03:18 -0300

    feat: adds localStorage functions

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 class User {
+
     constructor(name, gender, birth, country, email, password, photo, admin){
+
+        this._id;
         this._name = name;
         this._gender = gender;
         this._birth = birth;
@@ -9,9 +12,18 @@ class User {
         this._photo = photo;
         this._admin = admin;
         this._register = new Date();
+
     }
-     // Getters
-     get name() {
+
+    get id(){
+        return this._id;
+    }
+
+    get register(){
+        return this._register;
+    }
+
+    get name(){
         return this._name;
     }
 
@@ -31,57 +43,115 @@ class User {
         return this._email;
     }
 
-    get password() {
-        return this._password;
-    }
-
     get photo() {
         return this._photo;
+    }
+
+    get password() {
+        return this._password;
     }
 
     get admin() {
         return this._admin;
     }
 
-    get register(){
-        return this._register;
-    }
-
-    // Setters
-    set name(value) {
-        this._name = value;
-    }
-
-    set gender(value) {
-        this._gender = value;
-    }
-
-    set birth(value) {
-        this._birth = value;
-    }
-
-    set country(value) {
-        this._country = value;
-    }
-
-    set email(value) {
-        this._email = value;
-    }
-
-    set password(value) {
-        this._password = value;
-    }
-
-    set photo(value) {
+    set photo(value){
         this._photo = value;
     }
 
-    set admin(value) {
-        this._admin = value;
+    loadFromJSON(json){
+
+        for (let name in json){
+            
+            switch(name){
+
+                case '_register':
+                    this[name] = new Date(json[name]);
+                break;
+                default:
+                    this[name] = json[name];
+
+            }
+            
+
+        }
+
     }
 
-    set register(value){
-        this._register = value;
+    static getUsersStorage() {
+
+        let users = [];
+
+        if (localStorage.getItem("users")) {
+
+            users = JSON.parse(localStorage.getItem("users"));
+
+        }
+
+        return users;
+
     }
+
+    getNewID(){
+
+        let usersID = parseInt(localStorage.getItem("usersID"));
+
+        if (!usersID > 0) usersID = 0;
+
+        usersID++;
+
+        localStorage.setItem("usersID", usersID);
+
+        return usersID;
+
+    }
+
+    save(){
+
+        let users = User.getUsersStorage();
+
+        if (this.id > 0) {
+            
+            users.map(u=>{
+
+                if (u._id == this.id) {
+
+                    Object.assign(u, this);
+
+                }
+
+                return u;
+
+            });
+
+        } else {
+
+            this._id = this.getNewID();
+
+            users.push(this);
+
+        }
+
+        localStorage.setItem("users", JSON.stringify(users));
+
+    }
+
+    remove(){
+
+        let users = User.getUsersStorage();
+
+        users.forEach((userData, index)=>{
+
+            if (this._id == userData._id) {
+
+                users.splice(index, 1);
+
+            }
+
+        });
+
+        localStorage.setItem("users", JSON.stringify(users));
+
+    }
+
 }
-
